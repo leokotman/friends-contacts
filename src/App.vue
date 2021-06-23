6648a05c2bfabb597f700f0f2980b12026cfaecd
levkotman <leoktmn@yandex.ru>
2021-06-23 17:47:22 +0200

    added props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <ul class="contacts_list">
+    <friend-contacts v-for="friend in friends" :key="friend.id" :name="friend.name" :phone="friend.phone" :email="friend.email"/>
+  </ul>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FriendContacts from "./components/FriendContacts.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    FriendContacts,
+  },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "0123 45678 90",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Jones",
+          phone: "4952 22304 12",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +42,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.contacts_list,
+.contacts_list li {
+  list-style-type: none;
 }
 </style>
